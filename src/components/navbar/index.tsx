280959687447ad941a98517e703854a432032fb8
anaalbsantos <6030.anaalbuquerque@gmail.com>
2025-05-25 16:05:01 -0300

    enhance NavBar with smooth scrolling to sections

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,9 +2,12 @@ import { LareLogo } from "../../assets";
 import { Search } from "lucide-react";
 import { Cart } from "..";
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,6 +22,30 @@ const NavBar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const handleProductsClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    if (location.pathname !== "/") {
+      navigate("/");
+      // Aguarda a navegação ser concluída antes de fazer o scroll
+      setTimeout(() => {
+        document
+          .getElementById("our-products")
+          ?.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    } else {
+      document
+        .getElementById("our-products")
+        ?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handleTalkToUsClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    document.getElementById("footer")?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div
       className={`flex flex-row w-full items-center justify-between h-16 text-dark py-4 px-8 sticky top-0 z-10 transition-all duration-300 ease-in-out ${
@@ -30,10 +57,18 @@ const NavBar = () => {
         <h2 className="font-bold">Larê</h2>
       </div>
       <div className="flex flex-row gap-4 font-medium text-[15px]">
-        <a href="#our-products" className="text-dark hover:text-borrow">
+        <a
+          href="#our-products"
+          onClick={handleProductsClick}
+          className="text-dark hover:text-borrow"
+        >
           Produtos
         </a>
-        <a href="#footer" className="text-dark hover:text-borrow">
+        <a
+          href="#footer"
+          className="text-dark hover:text-borrow"
+          onClick={handleTalkToUsClick}
+        >
           Fale conosco
         </a>
       </div>
